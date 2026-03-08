aab0eb999441b932fa16ede253627976ef5ebee4
Nikoloz Kononov <ns4844752@gmail.com>
2026-03-08 22:27:34 +0400

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -8,3 +8,33 @@ document.querySelectorAll("a").forEach(link => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function(){
+const buttons = document.querySelectorAll(".skills-grid .skill")
+const windows = document.querySelectorAll(".skills-grid .skillWindow")
+buttons.forEach((button, index) => {
+  const window = windows[index] 
+
+  button.addEventListener("click", () => {
+    window.classList.add("show")
+  })
+  
+  const closeBtn = window.querySelector(".closeBtn")
+  closeBtn.addEventListener("click", () => {
+    window.classList.remove("show")
+  })
+
+window.addEventListener("click", (e) => {
+  if(e.target === window){
+      window.classList.remove("show")
+    }
+  })
+
+})
+
+document.addEventListener("keydown", (e) => {
+  if(e.key === "Escape"){
+    windows.forEach(win => win.classList.remove("show"))
+  }
+})
+})
